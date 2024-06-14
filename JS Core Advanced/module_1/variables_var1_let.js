f00829105cfdb05e8e1694f4e56bd2733805823c
NovikMaria <147400615+NovikMaria@users.noreply.github.com>
2024-06-14 13:29:03 +0300

    Delete JS Core Advanced/module_1 directory

--- JS Core Advanced/module_1/variables_var1_let.js
+++
@@ -1,9 +0,0 @@
-function foo() {
-    {
-      let x = 2;
-      console.log(x); // Выводит 2
-    }
-    console.log(x + "2"); // Ошибка: x не определена
-  }
-  
-  foo();
--- JS Core Advanced/module_1/variables_var1_var.js
+++
@@ -1,9 +0,0 @@
-function foo() {
-    {
-      var x = 1;
-      console.log(x); // Выводит 1
-    }
-    console.log(x + "1"); // Выводит 11
-  }
-  
-  foo();
